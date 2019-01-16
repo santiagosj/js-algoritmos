1753adefc478a73d64409e47a21146b1ede070e4
Santiago Spinetto Jung <sanjs965@gmail.com>
2019-01-16 10:51:29 -0300

    merge y seleccion

--- Algoritmos_ordenamiento_busqueda/mergeSort.js
+++ Algoritmos_ordenamiento_busqueda/mergeSort.js
@@ -1,5 +1,13 @@
 //MERGE SORT O ORDENAMIENTO POR MEZCLA
-
+/*
+1-Si la longitud de la lista es 0 o 1, ya está ordenada;
+2-Dividr la lista desordenada en dos sublistas de la mitad del tamaño o aprox;
+3-volver a dividir las dos listas para obtener 4 sublistas
+4-dividir las 4 listas en dos para hasta obtener valores individuales
+5-comparar parejas y armar de menor a mayor parejas de dos
+6 comparar parejas de a dos y armar sublistas de 4 de menor a mayor
+7-comparar estas dos ulitmas listas y ordenarlas en una de menor a mayor
+*/
 function merge(left, right, arr) {
   var a = 0;
 
--- Algoritmos_ordenamiento_busqueda/seleccionDirecta.js
+++ Algoritmos_ordenamiento_busqueda/seleccionDirecta.js
@@ -1,5 +1,22 @@
 //ORDENAMIENTO POR SELECCION DIRECTA
 
+/*
+1-busca le valor minimo de la lista;
+2-intercambia con el primero;
+3-busca el siguiente valor minimo;
+4-lo intercambia con el segundo;
+
+para i=0 hasta n-1
+    mínimo = i;
+    para j=i+1 hasta n
+        si lista[j] < lista[mínimo] entonces
+            mínimo = j // (!)
+        fin si
+    fin para
+    intercambiar(lista[i], lista[mínimo])
+fin para
+*/
+
 function selectionSort(nums) {
   var len = nums.length;
   for(var i = 0; i < len; i++) {
