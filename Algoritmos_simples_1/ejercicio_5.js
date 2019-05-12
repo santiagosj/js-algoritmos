46f5f33aa290012e20abe208cef5f99597a40c76
Santiago Spinetto Jung <sanjs965@gmail.com>
2019-05-12 08:44:12 -0300

    fibonacci

--- Algoritmos_simples_1/ejercicio_5.js
+++ Algoritmos_simples_1/ejercicio_5.js
@@ -1,23 +1,62 @@
-// Sumar todos los numeros impares de la secuencia fibonacci menores que el parametro de la funcion
-// ---iteracion o recurcion---
+//fibonacci
+// ---iteracion o recursion---
 
-// expresion fibonacci iterativa: 
+// expresion fibonacci iterativa: devuelve los primeros n numeros de la secuencia
 
-function fib(n){
+function fibIterativa(num){
     let arr = [0, 1];
-    for (let i = 2; i < n + 1; i++){
-      arr.push(arr[i - 2] + arr[i -1])
+
+    for (let i = 2; i < num; i++){
+       arr.push(arr[i - 1] + arr[i - 2])
     }
-   return arr[n]
+
+   return arr
   }
 
-//expresion fibonacci recursiva:
+//expresion fibonacci recursiva: devuelve los primeros n numeros de la secuencia
 
-function fib(n) {
-    if (n < 2){
-      return n
+function fibRecursiva(num) {
+    if (num < 2){
+      return num
     }
-    return fib(n - 1) + fib (n - 2)
+    return fibRecursiva(num - 1) + fibRecursiva(num - 2)
   }
 
+//Fibonacci inversa algoritmo iterativo
+/*
+1)Declarar un array de tamaño n
+2)Inicializar a[0] = 0 y a[1] = 1;
+3)Hacer un loop desde 2 hasta n - 1 y almacenar la suma de a[i - 2] en a[i]
+4)Imprimir el array en reversa
+*/ 
+function fibonacciReverse(num){
+
+  let arr = [0, 1];
+
+  for (let i = 2; i < num; i++){
+      arr.push(arr[i-1] + arr[i-2])
+  }
+
+  return arr.reverse()
+}
+
+fibonacciReverse(16)
+
+function sumaFiboImpares(num){
+  var arr = [0, 1];
   
+  var result = 0;
+
+  while (arr[1] <= num) {
+      //chequea si es impar
+      if (arr[1] % 2 !== 0) {
+          result += arr[1];
+      }
+
+      arr[1] += arr[0];
+      arr[0] = arr[1] - arr[0];
+  }
+
+  return result;
+  
+}
