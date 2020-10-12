7fc261e8bea29fcbf2f6e2dbae92e31ff22371cd
santiagosj <sanjs965@gmail.com>
2020-10-12 20:29:29 -0300

    chuck norrins API

---
+++ Design_Patterns/b-2-Async/basic_example_3.js
@@ -0,0 +1,12 @@
+fetch('https://api.chucknorris.io/jokes/random')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+   // console.log("Data is: ", data)
+   var joke = data.value
+   console.log('JOKE: ',joke)
+})
+.catch((err)=>{
+    console.error(err)
+})
